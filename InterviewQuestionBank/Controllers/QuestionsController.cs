79d31ca19b255692f7beaf06147f17157dd01175
Rahul-50 <rahulkadam9692@gmail.com>
2025-06-22 12:16:05 +0530

    new changes

--- InterviewQuestionBank/Controllers/QuestionsController.cs
+++ InterviewQuestionBank/Controllers/QuestionsController.cs
@@ -68,7 +68,8 @@ public class QuestionsController : Controller
     //
     private List<Category> GetCategories()
     {
-        var dt = _db.GetData("SELECT * FROM Categories");
+        //var dt = _db.GetData("SELECT * FROM Categories");
+        var dt = _db.GetData("SELECT Id,Name FROM Categories");
         var list = new List<Category>();
         foreach (DataRow row in dt.Rows)
         {
